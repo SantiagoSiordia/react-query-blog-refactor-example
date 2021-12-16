44c169a16224751bd518f58b0561f73e26af5234
Santiago Siordia <santiago.gonzalez.siordia@gmail.com>
2021-12-16 14:21:02 -0600

    Dedupe Simultaneous Requests

--- hooks/usePost.js
+++ hooks/usePost.js
@@ -1,34 +1,55 @@
 import React from 'react'
 import axios from 'axios'
 
-export default function usePost(postId) {
-  const [post, setPost] = React.useState()
+const context = React.createContext()
+
+export function PostsContext({ children }) {
+  const [posts, setPosts] = React.useState([])
   const [error, setError] = React.useState()
   const [status, setStatus] = React.useState('loading')
 
-  const refetch = async () => {
-    try {
-      setStatus('loading')
+  const activePromiseRef = React.useRef(false)
 
-      const post = await axios
-        .get(`/api/posts/${postId}`)
-        .then((res) => res.data)
-
-      setPost(post)
-      setError()
-      setStatus('success')
-    } catch (err) {
-      setError(err)
-      setStatus('error')
+  const refetch = () => {
+    if (!activePromiseRef.current) {
+      activePromiseRef.current = (async () => {
+        try {
+          setStatus('loading')
+          const posts = await axios.get('/api/posts').then((res) => res.data)
+          setPosts(posts)
+          setError()
+          setStatus('success')
+        } catch (err) {
+          setError(err)
+          setStatus('error')
+        } finally {
+          activePromiseRef.current = false
+        }
+      })()
     }
+
+    return activePromiseRef.current
   }
+
+  const contextValue = React.useMemo(() => ({
+    posts,
+    status,
+    error,
+    refetch,
+  }))
+
+  return <context.Provider value={contextValue}>{children}</context.Provider>
+}
+
+export default function usePosts() {
+  const { posts, status, error, refetch } = React.useContext(context)
 
   React.useEffect(() => {
     refetch()
   }, [])
 
   return {
-    post,
+    posts,
     status,
     error,
     refetch,
